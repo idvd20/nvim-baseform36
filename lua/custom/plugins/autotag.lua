5d45842300a5dc89ba637e10561c281bb094a2d1
Dave <idvdivinagracia@gmail.com>
2026-05-11 15:22:37 +0800

    feat: add nvim-ts-autotag for JSX/HTML tag autoclose

---
+++ lua/custom/plugins/autotag.lua
@@ -0,0 +1,6 @@
+-- nvim-ts-autotag: auto-close JSX/TSX/HTML tags
+-- Type <div> and it inserts </div> automatically.
+
+vim.pack.add { 'https://github.com/windwp/nvim-ts-autotag' }
+
+require('nvim-ts-autotag').setup {}
